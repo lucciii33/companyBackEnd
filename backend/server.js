9ab862542617500b4b1019a068aa15dbad2ad039
Angelo Maiele <angelomaiele@gmail.com>
2022-11-05 15:00:47 -0400

    new proyect

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const port = process.env.PORT || 5000
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require("cors")
+const connectDB = require('./config/db')
+
+connectDB()
+
+app.use
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
